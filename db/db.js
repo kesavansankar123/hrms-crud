926f6d9cafe64f983f6fa6fbe795c3c6a0bc350c
kesavansankar123 <kasavansankar@gmail.com>
2023-10-24 03:06:01 -0700

    first commit

--- db/db.js
+++ db/db.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const Mongoose =require('mongoose');
 const loginUsers = require('../modals/attendance schema');
-const url ="mongodb+srv://admin:gokul@mern.sqrvp1s.mongodb.net/?retryWrites=true&w=majority"
+// const url ="mongodb+srv://admin:gokul@mern.sqrvp1s.mongodb.net/?retryWrites=true&w=majority"
 // const url="mongodb://127.0.0.1:27017/employee"
 
  function loginDetails() {
-    Mongoose.connect(url) 
+    Mongoose.connect(process.env.url) 
 
     Mongoose.connection.once('open',()=>{
         console.log('connected success');
